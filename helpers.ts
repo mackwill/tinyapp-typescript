18894c33d30c05c4e2fb4562de5769b78da6ec35
Will <mack.mindenhall@gmail.com>
2020-09-28 09:52:22 -0400

    Converted all helper functions to typescript

--- helpers.ts
+++ helpers.ts
@@ -1,5 +1,26 @@
 const bcrypt = require("bcrypt");
 
+interface IDatabaseObject {
+  longURL: string;
+  userID: string | null;
+  dateCreated: string;
+  visits: number;
+}
+
+interface IDatabase {
+  [key: string]: IDatabaseObject;
+}
+
+interface IUsersObject {
+  id: string;
+  email: string;
+  password: string;
+}
+
+interface IUsers {
+  [key: string]: IUsersObject;
+}
+
 // Generate a random string to assign to the shortened URL
 const generateRandomString = (length: number): string => {
   return Math.random().toString(36).substr(2, length);
@@ -14,18 +35,28 @@ const createCurrentDate = (): string => {
 // Check if user with provided email is in the database
 const findUserByEmail = (
   providedEmail: string,
-  database: object
+  users: IUsers
 ): string | undefined => {
-  for (let user in database) {
-    if (database[user].email === providedEmail) {
+  for (let user in users) {
+    if (users[user].email === providedEmail) {
       return user;
     }
   }
   return undefined;
 };
 
-const urlsForUser = (id, database) => {
-  let filteredURLs = {};
+interface IFilteredURL {
+  longURL: string | "";
+  created: string | "";
+  numVisits: number | null;
+}
+
+const urlsForUser = (id: string, database: IDatabase): IFilteredURL => {
+  let filteredURLs: IFilteredURL = {
+    longURL: "",
+    created: "",
+    numVisits: null,
+  };
 
   Object.keys(database).forEach((url) => {
     if (database[url].userID === id) {
@@ -40,7 +71,12 @@ const urlsForUser = (id, database) => {
   return filteredURLs;
 };
 
-const checkForExistingShortURL = (shortURL, database, users, id) => {
+const checkForExistingShortURL = (
+  shortURL: string,
+  database: object,
+  users: IUsers,
+  id: string
+): object => {
   let templateVars = {};
 
   if (database[shortURL] === undefined) {
@@ -59,19 +95,25 @@ const checkForExistingShortURL = (shortURL, database, users, id) => {
   return templateVars;
 };
 
-const authenticateUser = (email, database, plainPass) => {
+const authenticateUser = (
+  email: string,
+  database: IUsers,
+  plainPass: string
+): string | true => {
   const selectedUser = findUserByEmail(email, database);
   if (selectedUser === undefined) {
-    let msg = "You do not seem to have an account with us.";
-    return msg;
+    return "You do not seem to have an account with us.";
   } else if (!bcrypt.compareSync(plainPass, database[selectedUser].password)) {
-    let msg = `The provided details do not match our records.`;
-    return msg;
+    return `The provided details do not match our records.`;
   }
   return true;
 };
 
-const registerUser = (email, plainPass, database) => {
+const registerUser = (
+  email: string,
+  plainPass: string,
+  database: IUsers
+): string | true => {
   if (email === "" || plainPass === "") {
     return "Please fill in all fields.";
   } else if (findUserByEmail(email, database)) {
@@ -80,7 +122,11 @@ const registerUser = (email, plainPass, database) => {
   return true;
 };
 
-const isValidShortUrl = (user, database, shortURL) => {
+const isValidShortUrl = (
+  user: IUsersObject,
+  database: IDatabase,
+  shortURL: string
+): string | true => {
   if (user === undefined) {
     return `Please log in to view your shortURLs`;
   }
